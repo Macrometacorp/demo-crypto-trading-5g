4bf0b10b535691b396964cc6c6611a53eea9f15b
Tushar Pawar <tushar.pawar@macrometa.com>
2021-08-23 18:58:19 +0530

    Updates

---
+++ precache-manifest.6708d91db2dcc059966c5b4eae56cb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c43f28e5fc02813a2f9",
+    "url": "/demo-crypto-trading-5g/static/css/main.562614de.chunk.css"
+  },
+  {
+    "revision": "4c43f28e5fc02813a2f9",
+    "url": "/demo-crypto-trading-5g/static/js/main.4c43f28e.chunk.js"
+  },
+  {
+    "revision": "ee76c2f56f431e41d693",
+    "url": "/demo-crypto-trading-5g/static/js/1.ee76c2f5.chunk.js"
+  },
+  {
+    "revision": "25b31b64964897f9d6fc",
+    "url": "/demo-crypto-trading-5g/static/js/runtime~main.25b31b64.js"
+  },
+  {
+    "revision": "596666c9b38e421cedb8d17a4b3a59af",
+    "url": "/demo-crypto-trading-5g/static/media/logo.596666c9.svg"
+  },
+  {
+    "revision": "b2c282bca3cbe2d3abe104beab773ec5",
+    "url": "/demo-crypto-trading-5g/index.html"
+  }
+];
